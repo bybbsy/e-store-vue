c227e09d13eab38aa8aeeb32eae3f151f1475010
bybbsy <jimmy_boi@mail.ru>
2022-02-03 01:13:44 +0300

    Added app name

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    publicPath: '/e-store-vue/'
+    publicPath: '/e-store-vue/',
+    chainWebpack: config => {
+        config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = 'E-store'
+          return args
+        })
+      }
 }
