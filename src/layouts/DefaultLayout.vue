6b69390b25dda4d428d8e88c31495b326badee83
bybbsy <jimmy_boi@mail.ru>
2022-02-04 02:34:15 +0300

    Added NAvbar

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="default-wrapper">
+        <TheNavbar/>
         <router-view/>
     </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+import TheNavbar from '../components/TheNavbar/TheNavbar.vue';
+
 export default Vue.extend({
-    name: 'default-layout'
+    name: 'default-layout',
+    components: {
+        TheNavbar
+    }
 })
 </script>
+
+<style>
+.default-wrapper {
+    display: flex;
+    flex: 1 1 auto;
+}
+
+</style>
