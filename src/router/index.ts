f35775f775dd5c12d76d828f9c39f82720e87e78
bybbsy <jimmy_boi@mail.ru>
2022-02-04 02:34:53 +0300

    Added some styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,7 +14,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/about',
     name: 'About',
-    meta: { layout: 'default'},
+    meta: { layout: 'default' },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -23,25 +23,25 @@ const routes: Array<RouteConfig> = [
   {
     path: '/auth',
     name: 'Auth',
-    meta: { layout: 'default'},
+    meta: { layout: 'default' },
     component: () => import('../views/AuthPage.vue')
   },
   {
     path: '/coupons',
     name: 'Coupons',
-    meta: { layout: 'default'},
+    meta: { layout: 'default' },
     component: () => import('../views/CouponsPage.vue')
   },
   {
     path: '/products',
     name: 'Products',
-    meta: { layout: 'default'},
+    meta: { layout: 'default' },
     component: () => import('../views/ProductsPage.vue')
   },
   {
     path: '*',
     name: 'Error',
-    meta: { layout: 'empty'},
+    meta: { layout: 'empty' },
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
