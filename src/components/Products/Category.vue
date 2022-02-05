f3a689f82456bfb0890486dca3d699f8439e9eec
bybbsy <jimmy_boi@mail.ru>
2022-02-05 03:56:41 +0300

    Added styles

--- src/components/Products/Category.vue
+++ src/components/Products/Category.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class=""></div>
+  <div class="category-block">
+    <div class="category-wrapper">
+      <div class="category-name">Small toys</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,5 +14,31 @@ export default Vue.extend({
 </script>
 
 <style>
+.category-block {
+  display: inline-flex;
+}
 
+.category-block:not(:first-child) {
+  margin-left: 15px;
+}
+
+.category-wrapper {
+  position: relative;
+  background-color: transparent;
+  border: 1px solid #46444B;
+  color: #46444B;
+  padding: 10px 20px;
+  line-height: 150%;
+  border-radius: 50px;
+  transition: 0.1s all ease-in-out;
+  cursor: pointer;
+  margin: -1px;
+}
+
+.category-wrapper:hover,
+.category-wrapper:active {
+  margin: -2px;
+  border: 2px solid #fff;
+  color: #fff;
+}
 </style>
