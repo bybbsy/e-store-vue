3874c7e7065042bce78c613bb26be874a9aa86b4
bybbsy <jimmy_boi@mail.ru>
2022-02-08 01:24:00 +0300

    Added i18n plugin

---
+++ src/plugins/lang/i18n.ts
@@ -0,0 +1,19 @@
+import en from '@/resources/lang/en.json';
+import de from '@/resources/lang/de.json';
+import es from '@/resources/lang/es.json';
+import ru from '@/resources/lang/ru.json';
+
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+
+Vue.use(VueI18n);
+
+export default new VueI18n({
+  locale: localStorage.getItem('lang') || 'ru',
+  messages: {
+    en,
+    de,
+    es,
+    ru
+  }
+})
