09cc0405ca01bfccfd19be0711cf3062ff29ca7c
bybbsy <jimmy_boi@mail.ru>
2022-02-10 05:04:58 +0300

    Registered products module

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,17 +2,20 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import { State as AuthState } from '@/types/store/auth/state-types';
+import { State as ProductsState } from '@/types/store/products/state-types';
 
 import { moduleAuth } from './auth';
+import { moduleProducts } from './products'
 
 export type RootState = {
-  auth: AuthState
+  auth: AuthState,
+  products: ProductsState
 }
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    moduleAuth
+    moduleAuth,
+    moduleProducts
   }
 })
