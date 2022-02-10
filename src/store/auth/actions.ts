2744df3fb970b6a06ec3c5b8f87c709ed611e187
bybbsy <jimmy_boi@mail.ru>
2022-02-10 09:00:43 +0300

    Added correct actions

--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -3,29 +3,22 @@ import { State } from "@/types/store/auth/state-types";
 import { ActionTree } from "vuex";
 import { RootState } from "..";
 import firebase from "firebase/compat";
-import Vue from "vue";
 
 export const actions: ActionTree<State, RootState> & Actions = {
   async [ActionTypes.login]({commit}, {email, password}) {
-
-    Vue.prototype.$load(async () => {
-      await firebase.auth().signInWithEmailAndPassword(email, password);
-    });
-
+    await firebase.auth().signInWithEmailAndPassword(email, password);
   },
   async [ActionTypes.logout]() {
     await firebase.auth().signOut();
   },
   async [ActionTypes.register]({commit}, {email, password, firstName, lastName}) {
-      Vue.prototype.$load(async () => {
-        await firebase.auth().createUserWithEmailAndPassword(email, password);
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
 
-        const uid = firebase.auth().currentUser?.uid ?? null;
+    const uid = firebase.auth().currentUser?.uid ?? null;
 
-        await firebase.database().ref(`/users/${uid}/info`).set({
-          firstName,
-          lastName
-        })
-      })
+    await firebase.database().ref(`/users/${uid}/info`).set({
+      firstName,
+      lastName
+    })
   }
 }
