35d7c36c7a73cb2a42ffd9b74fd333c7ce5a7467
bybbsy <jimmy_boi@mail.ru>
2022-02-13 14:38:25 +0300

    Added plugin

---
+++ src/plugins/fontAwesomeIcons.ts
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+library.add(faBars);
