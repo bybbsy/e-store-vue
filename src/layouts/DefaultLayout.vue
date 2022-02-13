dad056cb9d29bc5633814dc4e2e50f86cca38a75
bybbsy <jimmy_boi@mail.ru>
2022-02-13 14:39:45 +0300

    Added styles

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -8,11 +8,18 @@
 <script lang="ts">
 import Vue from 'vue';
 import TheNavbar from '../components/TheNavbar/TheNavbar.vue';
+import firebase from "firebase/compat/app";
+
 
 export default Vue.extend({
     name: 'default-layout',
     components: {
         TheNavbar
+    },
+    async mounted() {
+      let response = await firebase.database().ref(`/users/${firebase.auth().currentUser?.uid}/info`).get();
+      let user = response.exists() ? response.val() : null;
+      console.log(user)
     }
 })
 </script>
@@ -22,5 +29,4 @@ export default Vue.extend({
     display: flex;
     flex: 1 1 auto;
 }
-
 </style>
