8566d3d18e407ed0e64bd63c7bf7b5222f4a3c2d
Kirill P <jimmy_boi@mail.ru>
2022-02-18 12:34:21 +0300

    Added fontawesome icons

--- src/plugins/fontAwesomeIcons.ts
+++ src/plugins/fontAwesomeIcons.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import { library } from '@fortawesome/fontawesome-svg-core'
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
