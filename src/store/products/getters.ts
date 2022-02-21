794e163488c3aa90aced2ab515ddcd94b9b29f30
Kirill P <jimmy_boi@mail.ru>
2022-02-21 15:20:05 +0300

    Augmented getters

--- src/store/products/getters.ts
+++ src/store/products/getters.ts
@@ -1,9 +1,13 @@
+import { Getters } from "@/types/store/products/getters-types";
 import { State } from "@/types/store/products/state-types";
 import { GetterTree } from "vuex";
 import { RootState } from "..";
 
-export const getters: GetterTree<State, RootState> = {
+export const getters: GetterTree<State, RootState> & Getters = {
   getProducts(state) {
     return state.products;
+  },
+  getDetailsExpanded(state) {
+    return state.productDetails;
   }
 }
