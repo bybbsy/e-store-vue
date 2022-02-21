fb1f1fc5d6383ee5090349a4bf1a17fe6783c8d1
Kirill P <jimmy_boi@mail.ru>
2022-02-21 15:16:49 +0300

    Added types for receving and toggling details card

--- src/types/store/products/mutation-types.ts
+++ src/types/store/products/mutation-types.ts
@@ -1,9 +1,11 @@
 import { CardProduct, State } from "./state-types";
 
 export enum MutationTypes {
-  setProducts = 'SET_PRODUCTS'
+  setProducts = 'SET_PRODUCTS',
+  toggleDetails = "TOGGLE_DETAILS"
 }
 
 export type Mutations = {
   [MutationTypes.setProducts](state: State, payload: Array<CardProduct>): void
+  [MutationTypes.toggleDetails](state: State): void
 }
