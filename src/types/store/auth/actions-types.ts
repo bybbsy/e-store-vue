32057233dbe87b63d6ff78ebf1fd99f909ec43d9
Kirill P <jimmy_boi@mail.ru>
2022-02-22 18:16:59 +0300

    Added actions types

--- src/types/store/auth/actions-types.ts
+++ src/types/store/auth/actions-types.ts
@@ -1,15 +1,19 @@
 import { ActionContext } from "vuex";
-import { State, User, UserFull } from './state-types';
+import { State, User, UserData, UserFull } from './state-types';
 import { RootState } from '@/store/index';
 
 export enum ActionTypes {
   login = 'LOGIN',
   logout = "LOGOUT",
-  register = "REGISTER"
+  register = "REGISTER",
+  getUid = "getUid",
+  setUserData = "setUserData"
 }
 
 export interface Actions {
   [ActionTypes.login]({commit}: ActionContext<State, RootState>, payload: User): void,
-  [ActionTypes.logout](): void,
+  [ActionTypes.logout]({commit}: ActionContext<State, RootState>): void,
   [ActionTypes.register]({commit}: ActionContext<State, RootState>, payload: UserFull): void
+  [ActionTypes.getUid](): ( string | null)
+  [ActionTypes.setUserData]({commit}: ActionContext<State, RootState>, payload: UserData): void
 }
