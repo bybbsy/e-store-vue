2059d34fbff968e57940059731074cb05d10c2f2
Kirill P <jimmy_boi@mail.ru>
2022-02-24 17:23:03 +0300

    Added validation for signin page

--- src/views/signinPage.vue
+++ src/views/signinPage.vue
@@ -26,8 +26,16 @@
               <div class="field-block">
                 <label for="sign-in-email" class="input__label">{{ $t('email') }}</label>
                 <div class="input-block">
-                  <input type="email" name="e-mail" class="form__input form__input-email" id="sign-in-email" placeholder="John" v-model="userEmail">
+                  <input type="email"
+                         name="e-mail"
+                         class="form__input form__input-email"
+                         id="sign-in-email"
+                         placeholder="John"
+                         @blur="$v.userEmail.$touch()"
+                         v-model.trim="userEmail"
+                         >
                   <img src="~@/assets/img/form/form-email.png" alt="" class="input-img">
+                  <span class="input-block__error" v-if="emailIsInvalid">{{ $t('invalid_field') }}</span>
                 </div>
               </div>
             </div>
@@ -35,14 +43,26 @@
               <div class="field-block">
                 <label for="sign-in-password" class="input__label">{{ $t('password') }}</label>
                 <div class="input-block">
-                  <input type="password" name="password" class="form__input form__input-password" id="sign-in-password" placeholder="********" v-model="userPassword">
+                  <input type="password"
+                         name="password"
+                         class="form__input form__input-password"
+                         id="sign-in-password"
+                         placeholder="********"
+                         @blur="$v.userPassword.$touch()"
+                         v-model.trim="userPassword"
+                         >
                   <img src="~@/assets/img/form/form-password.png" alt="" class="input-img">
+                  <span class="input-block__error" v-if="$v.userPassword.$dirty">{{ $t('invalid_field') }}</span>
                 </div>
               </div>
             </div>
           </div>
           <div class="auth__control">
-            <input type="submit" :value="$t('sign_in_button')" class="form__input form__input_sign-in">
+            <input type="submit"
+                   :disabled="emailIsInvalid || passwordIsInvalid"
+                   :value="$t('sign_in_button')"
+                   class="form__input form__input_sign-in"
+                   >
           </div>
           <div class="auth__hint">
             <p class="hint__message">{{ $t('dont_have_an_accound') }}<router-link class="hint__link" :to="{name: 'sign-up'}">{{ $t('sign_up_title') }}</router-link></p>
@@ -55,14 +75,41 @@
 
 <script lang="ts">
 import { User } from '@/types/store/auth/state-types';
+import { email, required, minLength } from 'vuelidate/lib/validators';
 
 import Vue from 'vue';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 
 export default Vue.extend({
   data() {
     return {
       userEmail: '',
       userPassword: ''
+    }
+  },
+  validations: {
+    userEmail: {
+      required,
+      email,
+      minLength: minLength(6)
+    },
+    userPassword: {
+      required,
+      minLength: minLength(6)
+    }
+  },
+  computed: {
+    emailIsInvalid() {
+      if(this.$v.userEmail.$dirty) {
+        return this.$v.userEmail.$invalid;
+      }
+      return false;
+    },
+    passwordIsInvalid() {
+      if(this.$v.userPassword.$dirty) {
+        return this.$v.userPassword.$invalid;
+      }
+      return false;
     }
   },
   methods: {
@@ -83,13 +130,8 @@ export default Vue.extend({
 </script>
 
 <style>
+.input-block__error {
+  line-height: 150%;
+  color: var(--main-error);
+}
 </style>
-
-// {
-//     "name": "Rosemarie Macejkovic",
-//     "lastname": "Hansen",
-//     "password": "vJNKBRtxdllMJ96",
-//     "avatar": "http://placeimg.com/640/480",
-//     "email": "Terry30@hotmail.com",
-//     "id": "1"
-//   },
