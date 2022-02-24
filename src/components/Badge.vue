f3460d040cca3fdde5a4be2d17e728f8bbaf858b
Kirill P <jimmy_boi@mail.ru>
2022-02-24 15:30:15 +0300

    Added template for badge

---
+++ src/components/Badge.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="badge-block">
+    <span class="badge-info"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+
+}
+</script>
+
+<style>
+.badge-block {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 25px;
+  height: 25px;
+}
+
+.badge-info {
+  color: var(--main-white)
+}
+
+.badge-orange {
+  background-color: var(--main-orange);
+}
+
+</style>
