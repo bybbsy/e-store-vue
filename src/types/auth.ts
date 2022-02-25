c08329845f7d076327b3752fc6cbdd9d7e6543c5
Kirill P <jimmy_boi@mail.ru>
2022-02-25 17:20:03 +0300

    Added types for validation forms

---
+++ src/types/auth.ts
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import { Validation } from "vuelidate";
+import { ValidationProperties, ValidationGroups } from "vue/types/vue";
+
+// User to specify type of parameter this.$v
+type ValidationProp = ValidationProperties<Vue> & ValidationGroups & Validation;
+
+type formOptions<T> = {
+  [K in keyof T]: {
+    required: boolean,
+    minLength: number
+  }
+}
+
+interface SigninFormFields {
+  userEmail: string,
+  userPassword: string
+}
+
+interface SignupFormFields {
+  firstname: string,
+  lastname: string,
+  userEmail: string,
+  userPassword: string,
+  repeatUserPassword: string
+}
+
+type SigninFormOptions = formOptions<SigninFormFields>;
+
+type SignupFormOptions = formOptions<SignupFormFields>;
+
+export {
+  ValidationProp,
+  SigninFormFields,
+  SignupFormFields,
+  SigninFormOptions,
+  SignupFormOptions
+}
