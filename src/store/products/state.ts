f2654ddf8cb0209d8b05fa1ad3821f5fa412d48a
Kirill P <jimmy_boi@mail.ru>
2022-03-01 14:27:00 +0300

    Data for empty detailProduct provided by variables now

--- src/store/products/state.ts
+++ src/store/products/state.ts
@@ -1,17 +1,9 @@
 import { State } from '@/types/store/products/state-types';
+import { emptyDetailProduct } from '@/variables';
 
 export const state: State = {
   products: [],
-  currentProduct: {
-    // #TODO Add product ID
-    // #TODO Add default image while loading data
-    productID: '',
-    name: 'no-name',
-    imgLink: '',
-    rate: 0,
-    price: 0,
-    comments: []
-  },
+  currentProduct: emptyDetailProduct,
   productsCart: [],
   productDetails: false
 }
