62b78be09bc8ce6559b77dd9cb3dcd58d804fcf6
Kirill P <jimmy_boi@mail.ru>
2022-03-01 09:41:14 +0300

    Implementing adding product to the cart, removing

--- src/store/products/mutations.ts
+++ src/store/products/mutations.ts
@@ -1,5 +1,6 @@
 import { Mutations, MutationTypes } from "@/types/store/products/mutation-types";
 import { State } from "@/types/store/products/state-types";
+import _ from "lodash";
 import { MutationTree } from "vuex";
 
 export const mutations: MutationTree<State> & Mutations = {
@@ -11,5 +12,17 @@ export const mutations: MutationTree<State> & Mutations = {
   },
   [MutationTypes.setDetails](state, payload) {
     state.currentProduct = payload;
+  },
+  [MutationTypes.addToCart](state, payload) {
+    state.productsCart.unshift(payload);
+  },
+  [MutationTypes.increaseAmount](state, payload) {
+    payload.count += 1;
+  },
+  [MutationTypes.decreaseAmount](state, payload) {
+    payload.count -= 1;
+  },
+  [MutationTypes.deleteFromCart](state, payload) {
+    _.remove(state.productsCart, payload)
   }
 }
