d4c167a0bb17fedbf48903699da0745a7db87d19
Kirill P <jimmy_boi@mail.ru>
2022-03-01 14:26:19 +0300

    Added toggleDetails helper

--- src/helpers/useProducts.ts
+++ src/helpers/useProducts.ts
@@ -1,8 +1,15 @@
-import { CartProduct, Product } from "@/types/store/products/state-types";
+import { CartProduct, Product, DetailProduct } from "@/types/store/products/state-types";
 import { ProductOrNot } from "@/types/products";
+import store from '@/store/index';
 
 function isProductInACart(productsCart: Array<CartProduct>, payload: CartProduct | Product): ProductOrNot {
   return productsCart.find((el) => el.productID === payload.productID) ?? false;
 }
+async function toggleDetails(toggle: boolean, payload: (DetailProduct | Product)) {
+  if(toggle) {
+    store.dispatch('toggleDetails');
+  }
+  await store.dispatch('setDetails', payload);
+}
 
-export { isProductInACart };
+export { isProductInACart, toggleDetails };
