b9cb2567bb4c0fd82e6019fa179b21c2fa468c44
Kirill P <jimmy_boi@mail.ru>
2022-03-02 16:30:31 +0300

    Added helpers for sync data with firebase

--- src/helpers/useProducts.ts
+++ src/helpers/useProducts.ts
@@ -1,6 +1,9 @@
 import { CartProduct, Product, DetailProduct } from "@/types/store/products/state-types";
 import { ProductOrNot } from "@/types/products";
 import store from '@/store/index';
+import { userIsAuthorized } from "./auth";
+import firebase from "firebase/compat";
+import _ from "lodash";
 
 function isProductInACart(productsCart: Array<CartProduct>, payload: CartProduct | Product): ProductOrNot {
   return productsCart.find((el) => el.productID === payload.productID) ?? false;
@@ -12,4 +15,17 @@ async function toggleDetails(toggle: boolean, payload: (DetailProduct | Product)
   await store.dispatch('setDetails', payload);
 }
 
-export { isProductInACart, toggleDetails };
+function sendCartToFirebase(currentCart: Array<CartProduct>) {
+  const deb = _.debounce(async () => {
+    const userID = userIsAuthorized();
+
+    if(userID) {
+      const cart = JSON.stringify(currentCart);
+      await firebase.database().ref(`/users/${userID}/info/cart`).set(cart);
+    }
+  }, 250)
+
+  return deb()
+}
+
+export { isProductInACart, toggleDetails, sendCartToFirebase };
