a10d8d18c0b410f250575918d3e5612d26a7b9f5
Kirill P <jimmy_boi@mail.ru>
2022-03-02 14:07:07 +0300

    Added setting for vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,7 +2,16 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
+import colors from 'vuetify/lib/util/colors';
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        warning: colors.orange.accent3
+      }
+    }
+  }
 });
