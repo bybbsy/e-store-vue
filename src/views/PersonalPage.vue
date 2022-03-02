4269d9c7fa527696c14b9d3530ae8ab82d473249
Kirill P <jimmy_boi@mail.ru>
2022-03-02 14:08:35 +0300

    Added personal page

--- src/views/PersonalPage.vue
+++ src/views/PersonalPage.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="div">
-    <router-view/>
-  </div>
+  <v-app>
+    <div class="personal__wrapper wrapper--cover wrapper--dark _hide-scroll">
+      <div class="personal__container">
+        <router-view/>
+      </div>
+    </div>
+  </v-app>
 </template>
 
 <script lang="ts">
@@ -13,5 +17,8 @@ export default Vue.extend({
 </script>
 
 <style>
-
+.personal__container {
+  height: 100%;
+  padding: 20px;
+}
 </style>
