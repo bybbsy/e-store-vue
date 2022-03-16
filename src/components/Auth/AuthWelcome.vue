c1fd338adeb785e1e45a34dae9f121ddb01642ab
Kirill P <jimmy_boi@mail.ru>
2022-03-16 16:58:36 +0300

    Added AuthWelcome component

---
+++ src/components/Auth/AuthWelcome.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="left__content">
+   <div class="left__image">
+      <img src="~@/assets/img/base/Welcome.png" alt="Welcome image">
+    </div>
+    <div class="left__welcome-text">
+      <h2 class="title ">{{ title }}</h2>
+      <p class="welcome-text__description">{{ description }}</p>
+      <p class="welcome-text__description welcome-text__description_orange">or</p>
+      <p class="hint__message hint__message_white">Go to the <router-link :to="{ name: 'products'}" class="hint__link">main page</router-link> </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'auth-welcome',
+  props: [
+    'title',
+    'description'
+  ]
+})
+</script>
