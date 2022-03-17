aead7611f53e3908c44feca37960e2b99f3e8126
Kirill P <jimmy_boi@mail.ru>
2022-03-17 13:02:20 +0300

    Removed local styles, added BEM styling

--- src/components/TheNavbar/TheNavbar.vue
+++ src/components/TheNavbar/TheNavbar.vue
@@ -3,27 +3,27 @@
   <aside class="navbar">
       <div class="navbar__container">
           <div class="navbar__user-block">
-              <div class="user-block__body">
-                  <div class="user-block__icon">
+              <div class="navbar___body">
+                  <div class="navbar__avatar">
                       <img :src="userData.imgLink" alt="Avatar">
                   </div>
-                  <div class="user-block__username">
+                  <div class="navbar__username">
                       <p>{{ getUsername }}</p>
                   </div>
 
-                  <div class="user-block__extra">
+                  <div class="navbar__email">
                     <p>{{ userData.email }} </p>
                   </div>
               </div>
           </div>
           <BlockFilter/>
           <div class="navbar__auth-block">
-              <div class="auth-block__buttons" v-if="uid">
-                  <a href="" class="auth-block__button auth-block__button_sign-out" @click="logOut">Sign out</a>
+              <div class="navbar__buttons-group" v-if="uid">
+                  <a href="" class="navbar__button navbar__button_sign-out" @click="logOut">Sign out</a>
               </div>
-              <div class="auth-block__buttons" v-else>
-                  <router-link :to="{ name: 'sign-up'}" class="auth-block__button auth-block__button_sign-up">Sign up</router-link>
-                  <router-link :to="{ name: 'sign-in'}" class="auth-block__button auth-block__button_sign-in">Sign in</router-link>
+              <div class="navbar__buttons-group" v-else>
+                  <router-link :to="{ name: 'sign-up'}" class="navbar__button navbar__button_sign-up">Sign up</router-link>
+                  <router-link :to="{ name: 'sign-in'}" class="navbar__button navbar__button_sign-in">Sign in</router-link>
               </div>
           </div>
       </div>
@@ -61,119 +61,7 @@ export default Vue.extend({
 </script>
 
 <style>
-.navbar {
-  min-width: 300px;
-  background-color: var(--secondary-dark);
-  padding: 50px 0;
-  overflow: hidden scroll;
-  height: 100vh;
-  scrollbar-width: none;
-}
-
-.navbar::-webkit-scrollbar {
-  display: none;
-}
-
-.navbar__container {
-  width: 215px;
-  display: flex;
-  flex-direction: column;
-  margin: 0 auto;
-  min-height: 100%;
-}
-
-.navbar__user-block {
-  margin: 0 0 45px;
-}
-
-.user-block__username {
-  line-height: 150%;
-  /* text-align: center; */
-  font-weight: 500;
-  color: var(--main-white);
-  font-size: 1.2em;
-}
-
-.user-block__extra {
-  line-height: 150%;
-  color: var(--text-gray);
-}
-
-.user-block__icon {
-  position: relative;
-  margin: 0 auto 25px;
-  width: 96px;
-  height: 96px;
-  border-radius: 50%;
-  cursor: pointer;
-}
-
-.user-block__icon img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: inherit;
-}
-
-.navbar__auth-block {
-  display: flex;
-  flex: 1 1;
-}
-.auth-block__buttons {
-  display: flex;
-  flex-direction: column;
-  margin: auto 0 0;
-  width: 100%;
-  height: 100%;
-}
-
-.auth-block__button:not(:last-child) {
-  margin: 0 0 5px;
-}
-
-.auth-block__button {
-  width: 100%;
-  border-radius: 25px;
-  min-height: 50px;
-  color: var(--main-white);
-  line-height: 27px;
-  padding: 10px 18px;
-  font-size: 1.125em;
-  text-align: center;
-  transition: 0.2s all ease;
-}
-
-.auth-block__button:hover {
-  box-shadow: 0px 0px 2px 1px var(--main-white);
-}
-
-.auth-block__button_sign-up {
-  background-color: var(--main-orange);
-}
-
-.auth-block__button_sign-in {
-  background-color: var(--main-success);
-}
-
-.auth-block__button_sign-out {
-  background-color: var(--main-error);
-}
-
 @media screen and (max-width: 1080px) {
-  .navbar {
-    min-width: 150px;
-  }
-
-  .navbar__container {
-    width: 135px;
-  }
-
-  .user-block__icon {
-    width: 60px;
-    height: 60px;
-  }
 
   .filter-block__main-filter,
   .filter-element__name {
@@ -190,13 +78,5 @@ export default Vue.extend({
 }
 .navbar__expanded {
   margin-left: 0;
-}
-@media screen and (max-width: 980px) {
-  .navbar {
-    min-width: 300px;
-    margin-left: -300px;
-  }
-
-
 }
 </style>
