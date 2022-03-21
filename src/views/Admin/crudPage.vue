769ea0766556f8329c4d30dce6d51ade47300c12
Kirill P <jimmy_boi@mail.ru>
2022-03-21 17:36:46 +0300

    Added links for nav menu

--- src/views/Admin/crudPage.vue
+++ src/views/Admin/crudPage.vue
@@ -2,37 +2,26 @@
   <v-row>
     <v-col cols="2">
       <v-sheet rounded="lg">
-        <v-list-item-group
-          color="primary"
-          v-model="selectedItem"
-          >
-          <v-list-item
-            v-for="(action, index) in actions"
-            :key="index"
-            :to="{ name: action.link }"
-            link
-          >
-            <v-list-item-icon>
-                <v-icon v-text="action.icon"></v-icon>
-              </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="action.name"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-divider class="my-2"></v-divider>
-
-          <v-list-item
-            link
-            color="grey lighten-4"
-          >
-            <v-list-item-content>
-              <v-list-item-title>
-                Refresh
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
+        <v-list>
+          <v-list-item-group
+            color="primary"
+            v-model="selectedItem"
+            >
+            <v-list-item
+              v-for="(action, index) in actions"
+              :key="index"
+              :to="{ name: action.link }"
+              link
+            >
+              <v-list-item-icon>
+                  <v-icon v-text="action.icon"></v-icon>
+                </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="action.name"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
       </v-sheet>
     </v-col>
 
@@ -53,9 +42,10 @@ import Vue from 'vue';
 export default Vue.extend({
   data: () => {
     return {
-      selectedItem: '',
+      selectedItem: null,
       actions: [
-        { name: 'Add', link: 'crud-create', icon: 'mdi-plus-circle-outline' },
+        { name: 'Create', link: 'crud-create', icon: 'mdi-hamburger-plus' },
+        { name: 'Manage', link: 'crud-all', icon: 'mdi-food' }
       ]
     }
   },
