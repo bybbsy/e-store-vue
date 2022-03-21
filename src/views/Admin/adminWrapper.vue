89f5862a8842fd84d095962e998e215e962e9ef3
Kirill P <jimmy_boi@mail.ru>
2022-03-21 17:36:18 +0300

    Added admin wrapper

--- src/views/Admin/adminWrapper.vue
+++ src/views/Admin/adminWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="" >
     <v-app id="inspire">
     <v-app-bar
       app
@@ -7,13 +7,7 @@
       flat
     >
       <v-container class="py-0 fill-height">
-        <v-avatar
-          class="mr-10"
-          color="grey darken-1"
-          size="32"
-        ></v-avatar>
-
-        <v-tab
+          <v-btn
           v-for="(link, index) in links"
           :key="index"
           text
@@ -21,18 +15,31 @@
         >
           {{ link.name }}
           <v-badge v-if="link.beta" :content="link.betaContent"></v-badge>
-        </v-tab>
+        </v-btn>
 
         <v-spacer></v-spacer>
 
         <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-          ></v-text-field>
+          <v-row class="ma-0">
+            <v-list-item-avatar color="grey" size="32" rounded="50%">
+              <v-img :src="userData.imgLink"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>{{ getUsername }}</v-list-item-title>
+              <v-list-item-subtitle>{{ getEmail }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action class="ma-0">
+                <v-btn
+                icon
+                color="primary"
+                @click="logOut"
+                >
+                  <a href="">
+                    <v-icon size="25">mdi-exit-to-app</v-icon>
+                  </a>
+                </v-btn>
+            </v-list-item-action>
+          </v-row>
         </v-responsive>
       </v-container>
     </v-app-bar>
@@ -47,40 +54,48 @@
 </template>
 
 <script lang="ts">
-import { userIsAuthorized } from '@/helpers/auth';
-import store from '@/store';
-import firebase from 'firebase/compat';
+import { allowedUsers } from '@/variables';
 import _ from 'lodash';
 import Vue from 'vue'
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default Vue.extend({
   data: () => ({
     links: [
       { name: 'Dashboard', link: 'admin-main', },
       { name: 'Manage items', link: 'admin-crud',},
-      { name: 'Users', link: '', beta: true, betaContent: 'Soon'},
-    ],
+      { name: 'E-store', link: 'products'},
+      // { name: 'Users', link: 'q', beta: true, betaContent: 'Soon'},
+    ]
   }),
-  async created() {
-    const userID = userIsAuthorized();
-
-    if(userID) {
-      let response = (await firebase.database().ref(`/users/${userID}/info`).get()).val();
-
-      let userData = _.omit(response, ['cart'])
-
-      let cart = JSON.parse(response.cart);
-
-      this.setUserData(userData);
-      this.setUserCart(cart);
+  computed: {
+    ...mapGetters({
+      userData: 'getUserData'
+    }),
+    userIsAllowed() {
+      return allowedUsers.includes(this.userData.role) ?? false;
+    },
+    getUsername() {
+      if(this.userData.firstName) {
+        return `${this.userData.firstName} ${this.userData.lastName}`;
+      }
+      return 'Admin'
+    },
+    getEmail() {
+      if(this.userData.email) {
+        return this.userData.email;
+      }
+      return 'user@estore.admin.com';
     }
   },
   methods: {
     ...mapActions([
       'setUserData',
-      'setUserCart'
-    ])
+      'setUserCart',
+    ]),
+    ...mapActions({
+      logOut: 'LOGOUT'
+    })
   }
 })
 </script>>
