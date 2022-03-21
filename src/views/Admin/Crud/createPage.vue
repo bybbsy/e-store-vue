8a1bcacc078aa8cc0c05139dfedae29f5ff07929
Kirill P <jimmy_boi@mail.ru>
2022-03-21 17:37:47 +0300

    Added create page

--- src/views/Admin/Crud/createPage.vue
+++ src/views/Admin/Crud/createPage.vue
@@ -1,109 +1,144 @@
 <template>
   <form class="pa-5">
     <v-card-title class="pa-0">Create a new item</v-card-title>
-    <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
-      required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="description"
-      :error-messages="descriptionErrors"
-      :counter="100"
-      label="Description"
-      required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <v-row class="ma-0">
-      <v-list-item-avatar
-        tile
-        size="120"
-        color="grey"
-      >
-      <img v-if="imgLink.length > 0" :src="imgLink" alt="Product image">
-      </v-list-item-avatar>
-      <v-text-field
-      v-model="imgLink"
-      :error-messages="imgLinkErrors"
-      label="Image link"
-      required
-      @input="$v.imgLink.$touch()"
-      @blur="$v.imgLink.$touch()"
-    ></v-text-field>
+    <v-row>
+      <v-col cols="6">
+        <v-text-field
+          v-model="name"
+          :error-messages="nameErrors"
+          :counter="100"
+          label="Name"
+          required
+          @input="$v.name.$touch()"
+          @blur="$v.name.$touch()"
+        ></v-text-field>
+
+        <v-textarea
+          v-model="description"
+          :error-messages="descriptionErrors"
+          :counter="1000"
+          label="Description"
+          required
+          @input="$v.description.$touch()"
+          @blur="$v.description.$touch()"
+        >
+        </v-textarea>
+
+        <v-select
+          v-model="category"
+          :items="categories"
+          item-text="category"
+          label="Category"
+          persistent-hint
+          return-object
+        ></v-select>
+
+      </v-col>
+
+
+      <v-col cols="6">
+        <v-list-item-avatar
+          tile
+          size="120"
+          color="grey"
+        >
+          <v-img v-if="imgLink.length > 0" :src="imgLink" alt="Product image" contain></v-img>
+        </v-list-item-avatar>
+        <v-text-field
+          v-model="imgLink"
+          :error-messages="imgLinkErrors"
+          label="Image link"
+          required
+          @input="$v.imgLink.$touch()"
+          @blur="$v.imgLink.$touch()"
+        ></v-text-field>
+      </v-col>
     </v-row>
 
-    <v-row class="ma-0">
-      <v-text-field
-        class="mr-5"
-        v-model="price"
-        :error-messages="priceErrors"
-        label="Price"
-        required
-        @input="$v.price.$touch()"
-        @blur="$v.price.$touch()"
-      ></v-text-field>
+
+    <v-row>
+      <v-col cols="3" class="pt-0">
+        <v-text-field
+          class="mr-5"
+          v-model="price"
+          :error-messages="priceErrors"
+          label="Price"
+          required
+          @input="$v.price.$touch()"
+          @blur="$v.price.$touch()"
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="3" class="pt-0 orange--text" >
         <v-select
           v-model="select"
           :hint="`${select.currency}`"
           :items="currencyTypes"
           item-text="currency"
-          label="Select"
+          label="Currency"
           persistent-hint
           return-object
-          single-line
         ></v-select>
+      </v-col>
     </v-row>
 
     <v-row class="ma-0">
-      <v-select
-      v-model="category"
-      :items="categories"
-      item-text="category"
-      label="Category"
-      persistent-hint
-      return-object
-      single-line
-    ></v-select>
-    </v-row>
-    <v-btn
+      <v-btn
       class="mr-4"
       @click="submit"
       color="success"
+      large
+      outlined
     >
       submit
       <v-icon> mdi-plus-circle-outline </v-icon>
     </v-btn>
-    <v-btn @click="clear">
+    <v-btn
+      @click="clear"
+      color="primary"
+      outlined
+      large
+    >
       clear
       <v-icon>mdi-autorenew</v-icon>
     </v-btn>
+    </v-row>
+    <v-dialog
+      v-model="dialog.state"
+      persistent
+      width="300"
+      >
+      <v-card
+        color="primary"
+        dark
+      >
+        <v-card-text>
+          {{ dialog.text }}
+          <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </form>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { validationMixin } from 'vuelidate';
-import { required, maxLength, url, numeric } from 'vuelidate/lib/validators';
+import { required, maxLength, url, decimal, minValue } from 'vuelidate/lib/validators';
+import firebase from "firebase/compat";
 
 export default Vue.extend({
-  mixins: [validationMixin],
-  validations: {
-    name: { required, maxLength: maxLength(20) },
-    description: { required, maxLength: maxLength(100) },
-    imgLink: { required, url },
-    select: { required },
-    price: { required, numeric },
-    category: { required }
-  },
   data() {
     return {
-      categories: ['Food', 'Toys', 'Health'],
-      category: 'Food',
+      dialog: {
+        state: false,
+        text: 'Adding data to firestore'
+      },
+      categories: ['food', 'toys', 'health'],
+      category: 'food',
       currencyTypes: [
         { currency: '₽' },
         { currency: '$' }
@@ -114,6 +149,14 @@ export default Vue.extend({
       select: { currency: '₽' },
       price: 0
     }
+  },
+  validations: {
+    name: { required, maxLength: maxLength(100) },
+    description: { required, maxLength: maxLength(1000) },
+    imgLink: { required, url },
+    select: { required },
+    price: { required, decimal, minValue: minValue(0.1) },
+    category: { required }
   },
   computed: {
     selectErrors () {
@@ -138,22 +181,52 @@ export default Vue.extend({
     },
     imgLinkErrors() {
       const errors: Array<string> = []
-      if (!this.$v.description.$dirty) return errors
-      !this.$v.description.url && errors.push('This field should be url type.')
-      !this.$v.description.required && errors.push('Url to image is required.')
+      if (!this.$v.imgLink.$dirty) return errors
+      !this.$v.imgLink.url && errors.push('This field should be url type.')
+      !this.$v.imgLink.required && errors.push('Url to image is required.')
       return errors
     },
     priceErrors() {
       const errors: Array<string> = []
       if (!this.$v.price.$dirty) return errors
-      !this.$v.price.numeric && errors.push('This field should be numeric.')
+      !this.$v.price.decimal && errors.push('This field should be decimal.')
+      !this.$v.price.minValue && errors.push('This field should have a value at least 0.1')
       !this.$v.price.required && errors.push('This field is required.')
       return errors
     }
   },
   methods: {
-    submit () {
+    async submit () {
       this.$v.$touch()
+      if(!this.$v.$error) {
+        this.dialog.state = true;
+
+        const collectionRef = firebase.firestore().collection('products');
+
+        const formData = {
+          name: this.name,
+          description: this.description,
+          category: this.category,
+          price: Number(this.price),
+          currency: this.select.currency,
+          imgLink: this.imgLink,
+          rate: 0
+        }
+
+        const a = await collectionRef.add(formData)
+                                      // .catch(e => {
+                                      //   this.dialog.state = true;
+                                      //   this.dialog.text = e
+                                      // });
+
+        // https://toppng.com/uploads/preview/salad-with-chicken-png-chicken-caesar-salad-11563252871hjg6nsyeid.png
+
+        // const collectionRef = firebase.firestore().collection('products');
+        this.dialog.state = false
+        this.clear();
+        return;
+
+      }
     },
     clear () {
       this.$v.$reset()
